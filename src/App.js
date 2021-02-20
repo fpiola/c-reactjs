8ae9968110a6db70857c4065bf56909a0b05aaf3
fpiola <francis.piola@yahoo.com.br>
2021-02-20 11:16:01 -0300

    Incorporando Expressões em JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 
-//jsx
-const element = <h1>Hello, world!</h1>;
+//Incorporando Expressões em JSX
+const name = 'Francis Piola';
+const element = <h1>Hello, {name}</h1>;
 
 function App(){
     return (
@@ -15,6 +16,22 @@ function App(){
 export default App;
 
 /*
+--------------------------------------------
+
+--------------------------------------------
+//jsx
+const element = <h1>Hello, world!</h1>;
+
+function App(){
+    return (
+        <div>
+           <h1>{element}</h1>
+        </div>
+    )
+}
+
+export default App;
+--------------------------------------------
 function App(){
     const [count, setCount] = useState(0);
     return (
@@ -25,7 +42,7 @@ function App(){
         </div>
     )
 }
-
 export default App;
+--------------------------------------------
 
 */
