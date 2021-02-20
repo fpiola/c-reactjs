763fec406e728e92122762685424f15f6a61bc10
fpiola <francis.piola@yahoo.com.br>
2021-02-20 11:08:42 -0300

    jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
 
+
+//jsx
+const element = <h1>Hello, world!</h1>;
+
+function App(){
+    return (
+        <div>
+           <h1>{element}</h1>
+        </div>
+    )
+}
+
+export default App;
+
+/*
 function App(){
     const [count, setCount] = useState(0);
     return (
@@ -12,3 +27,5 @@ function App(){
 }
 
 export default App;
+
+*/
