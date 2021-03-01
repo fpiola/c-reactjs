f217bbf78ef2e699719f0cecef6e3a193d63295a
fpiola <francis.piola@yahoo.com.br>
2021-03-01 20:25:53 -0300

    define o título da página após o React atualizar o DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
+
+function App() {
+    const [count, setCount] = useState(0);
+    // Similar a componentDidMount e componentDidUpdate:  useEffect(() => {    // Atualiza o título do documento utilizando a API do navegador    document.title = `You clicked ${count} times`;  });
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+      </button>
+        </div>
+    );
+}
+
+export default App;
+
+/*
+
+--------------------------------------------
 
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
@@ -16,8 +35,6 @@ function App() {
 }
 
 export default App;
-
-/*
 
 
 --------------------------------------------
